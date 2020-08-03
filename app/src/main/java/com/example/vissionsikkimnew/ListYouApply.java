8f1c42577ab41386d7dbb2f18af1192335082559
Aasiy Zabeen <syedasiya000@gmail.com>
2020-08-03 14:22:42 +0530

    User side completed

--- app/src/main/java/com/example/vissionsikkimnew/ListYouApply.java
+++ app/src/main/java/com/example/vissionsikkimnew/ListYouApply.java
@@ -1,14 +1,98 @@
 package com.example.vissionsikkimnew;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListYouApply extends AppCompatActivity {
+
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference,DSchemes;
+    StorageReference storageReference;
+
+
+    //the listview
+    ListView listView;
+    AddScheme addScheme;
+
+
+
+    //
+//    //list to store uploads data
+//    List<AddScheme> uploadList;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_you_apply);
+
+
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Schemes");
+
+        addScheme = new AddScheme();
+
+        listView = findViewById(R.id.listView);
+        list= new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this,R.layout.scheme_info,R.id.SchemeInfo,list);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    addScheme = ds.getValue(AddScheme.class);
+                    list.add("Department Name : "+addScheme.getDepartmentName()+"\n"+"Scheme Name : "+addScheme.getSchemeName());
+                }
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
     }
 }
